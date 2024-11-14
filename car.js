3c9b8a5b846a36c33a6c42aa316a812832ae112e
Zulkifli Zaki <zaki@embedded.rwth-aachen.de>
2024-11-14 16:28:59 +0800

    Defining the road
    Define the road component by adding new Road and Utils classes.

--- car.js
+++ car.js
@@ -1,81 +1,71 @@
 class Car {
-    constructor (x, y, width, height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
 
-        this.speed = 0;
-        this.acceleration = 0.2;
-        this.maxSpeed = 3;
-        this.friction = 0.05;
-        this.angle = 0;
+    this.speed = 0;
+    this.acceleration = 0.2;
+    this.maxSpeed = 3;
+    this.friction = 0.05;
+    this.angle = 0;
 
-        this.controls = new Controls();
+    this.controls = new Controls();
+  }
+
+  update() {
+    this.#move();
+  }
+
+  #move() {
+    if (this.controls.forward) {
+      this.speed += this.acceleration;
+    }
+    if (this.controls.reverse) {
+      this.speed -= this.acceleration;
     }
 
-    update() {
-        this.#move();
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+    if (this.speed < -this.maxSpeed / 2) {
+      this.speed = -this.maxSpeed / 2;
     }
 
-    draw (context) {
-        context.save();
-        context.translate(this.x, this.y);
-        context.rotate(-this.angle);
-
-        context.beginPath();
-        context.rect (
-            -this.width/2,
-            -this.height/2,
-            this.width,
-            this.height
-        );
-        context.fill();
-
-        context.restore();
+    if (this.speed > 0) {
+      this.speed -= this.friction;
+    }
+    if (this.speed < 0) {
+      this.speed += this.friction;
+    }
+    if (Math.abs(this.speed) < this.friction) {
+      this.speed = 0;
     }
 
-    #move(){
-        if(this.controls.forward){
-            this.speed += this.acceleration;
-        }
-        if(this.controls.reverse){
-            this.speed -= this.acceleration;
-        }
-        
-        if(this.speed > this.maxSpeed){
-            this.speed = this.maxSpeed;
-        }
-
-        if(this.speed < -this.maxSpeed/2){ // -ve sign is a indicator of car moving backwards
-            this.speed = -this.maxSpeed/2; // reverse speed reduced to half
-        }
-        
-        if(this.speed > 0) {
-            this.speed -= this.friction; 
-        }
-
-        if(this.speed < 0){
-            this.speed += this.friction;
-        }
-
-        if(Math.abs(this.speed) < this.friction){
-            this.speed = 0;
-        }
-
-        if(this.speed != 0){
-            const flip = this.speed > 0 ? 1: -1;
-
-            if(this.controls.left){
-                this.angle += 0.03 * flip;
-            }
-    
-            if(this.controls.right){
-                this.angle -= 0.03 * flip;
-            }
-        }
-
-        this.x -= Math.sin(this.angle) * this.speed;
-        this.y -= Math.cos(this.angle) * this.speed;
+    if (this.speed != 0) {
+      const flip = this.speed > 0 ? 1 : -1;
+      if (this.controls.left) {
+        this.angle += 0.03 * flip;
+      }
+      if (this.controls.right) {
+        this.angle -= 0.03 * flip;
+      }
     }
+
+    this.x -= Math.sin(this.angle) * this.speed;
+    this.y -= Math.cos(this.angle) * this.speed;
+  }
+
+  draw(ctx) {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(-this.angle);
+
+    ctx.beginPath();
+    ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
+    ctx.fill();
+
+    ctx.restore();
+  }
 }
---
+++ utils.js
@@ -0,0 +1,4 @@
+// Linear interpolation
+function linearInterpolation(A, B, t) {
+  return A + (B - A) * t;
+}
